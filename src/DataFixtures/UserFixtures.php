35ac4bcb6e6e94adde6757dd6f4e0f31eabd37bc
Pablo Camara <pablo.camara@contentignition.com>
2019-04-16 03:01:49 +0100

    Basic User Login by Email and Password, UserFixtures with password encoder

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,14 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+
+public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setEmail('pablo@camara.pt');
-        $user->setPassword('ppi.pt2019');
+        $user->setPassword($this->passwordEncoder->encodePassword($user,'1234'));
         $manager->persist($user);
 
         $manager->flush();
