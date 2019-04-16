b3254d790901016e80e9814a9041e82e89a52bfa
Pablo Camara <pablo.camara@contentignition.com>
2019-04-16 02:51:04 +0100

    Login Security

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="app_login")
      */
-    public function index()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('login/index.html.twig', [
-            'error' => true,
-            'error_message' => 'Email ou password incorrecto(s)'
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
+
     }
 }
